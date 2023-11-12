ab1e6ea47d764c52e865a5b0fef8f25c94ecbf39
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-12 16:16:04 +0200

    fix8

--- _printf.c
+++ _printf.c
@@ -13,9 +13,7 @@
 int _printf(const char *format, ...)
 {
 	the_types fmt[] = {
-		{'c', print_char},
-		{'s', print_str},
-		{'\0', NULL}};
+		{'c', print_char}, {'s', print_str}, {'\0', NULL}};
 	int len, q, i;
 	va_list args;
 
@@ -25,6 +23,8 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	while (format && format[i] != '\0')
 	{
+		if (format[i + 1] == '\0')
+			return (-1);
 		if (format[i] == '%' && format[i + 1] == '%')
 		{
 			len += print_mod();
@@ -40,14 +40,12 @@ int _printf(const char *format, ...)
 					len += ((fmt[q]).f(args));
 					i += 1;
 				}
-				
 				q++;
 			}
 		}
 		else
 		{
 			len += _putchar(format[i]);
-			
 		}
 		i++;
 	}
