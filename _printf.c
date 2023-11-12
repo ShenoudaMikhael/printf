170d2fecf25b9e271be120824ff269d65c35a274
Salsabil <salsabeelahmed323@gmail.com>
2023-11-12 23:07:10 +0200

    editformat

--- _printf.c
+++ _printf.c
@@ -12,44 +12,14 @@
  */
 int _printf(const char *format, ...)
 {
-	the_types fmt[] = {
-		{'c', print_char}, {'s', print_str}, {'\0', NULL}};
-	int len, q, i;
+	int len;
 	va_list args;
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	len = 0, i = 0;
+	len = 0;
 	va_start(args, format);
-	while (format && format[i])
-	{
-		if (format[i] == '%')
-		{
-			while (format[i + 1] == ' ')
-				i++;
-			if (format[i + 1] == '%')
-			{
-				len += print_mod();
-				i += 2;
-				continue;
-			}
-			q = 0;
-			while (fmt[q].t != '\0')
-			{
-				if (fmt[q].t == format[i + 1])
-				{
-					len += ((fmt[q]).f(args));
-					i += 1;
-				}
-				q++;
-			}
-			if (fmt[q].t == '\0' && format[i] == '%')
-				len += print_mod();
-		}
-		else
-			len += _putchar(format[i]);
-		i++;
-	}
+	
 	va_end(args);
 	return (len);
 }
--- format.c
+++ format.c
@@ -1,5 +1,39 @@
 #include "main.h"
+#include <stddef.h>
 int _format(const char *format, va_list args)
 {
-
+	the_types fmt[] = {
+		{'c', print_char}, {'s', print_str}, {'\0', NULL}};
+	int len, q, i;
+	
+	len = 0, i = 0;
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			while (format[i + 1] == ' ')
+				i++;
+			if (format[i + 1] == '%')
+			{
+				len += print_mod();
+				i += 2;
+				continue;
+			}
+			q = 0;
+			while (fmt[q].t != '\0')
+			{
+				if (fmt[q].t == format[i + 1])
+				{
+					len += ((fmt[q]).f(args));
+					i += 1;
+				}
+				q++;
+			}
+			if (fmt[q].t == '\0' && format[i] == '%')
+				len += print_mod();
+		}
+		else
+			len += _putchar(format[i]);
+		i++;
+	}
 }
